40ad8791b3644acf5827009b99944dc9377df7cd
tatiana1990modelbock <tatianbock08@gmail.com>
2024-08-02 16:22:36 -0300

    Initial commit

---
+++ src/main/java/curso/dio/curso_dio_santander_2024/CursoDioSantander2024Application.java
@@ -0,0 +1,13 @@
+package curso.dio.curso_dio_santander_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 	CursoDioSantander2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoDioSantander2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/curso/dio/curso_dio_santander_2024/CursoDioSantander2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.dio.curso_dio_santander_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoDioSantander2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
